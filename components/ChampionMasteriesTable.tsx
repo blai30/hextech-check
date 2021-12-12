7d37dae951faec65e557bf12c5b9a96c830cbfc3
Brian Lai <brianlai30@outlook.com>
2021-12-11 23:26:37 -0800

    feat: add icon for no chest

--- components/ChampionMasteriesTable.tsx
+++ components/ChampionMasteriesTable.tsx
@@ -68,9 +68,13 @@ const ChampionMasteriesTable = ({ summonerName, region }: { summonerName: string
           <td className="px-6 py-4 whitespace-nowrap"
           >
             <span>
-              {championMastery.chestGranted && (
+              {championMastery.chestGranted ? (
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 text-amber-400 dark:text-amber-500" viewBox="0 0 20 20" fill="currentColor">
                   <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
+                </svg>
+              ) : (
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 text-gray-400 dark:text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636" />
                 </svg>
               )}
             </span>
