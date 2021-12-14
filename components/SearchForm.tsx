ce1884948466039489edce87a57c8fed393e7b48
Brian Lai <brianlai30@outlook.com>
2021-12-13 16:15:12 -0800

    fix: update color of region check mark for contrast

--- components/SearchForm.tsx
+++ components/SearchForm.tsx
@@ -105,7 +105,7 @@ const SearchForm = () => {
 
                             {selected ? (
                               <span
-                                className={`${active ? 'text-white' : 'text-indigo-600'} absolute inset-y-0 right-0 flex items-center pr-4`}
+                                className={`${active ? 'text-white' : 'text-indigo-600 dark:text-indigo-300'} absolute inset-y-0 right-0 flex items-center pr-4`}
                               >
                                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 13l4 4L19 7" />
