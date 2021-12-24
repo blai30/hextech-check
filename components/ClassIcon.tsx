e7cef2e20a79d80199c1a14dbce7c43f9ec8fe0b
Brian Lai <brianlai30@outlook.com>
2021-12-24 12:03:08 -0800

    fix: update class icon paths

--- components/ClassIcon.tsx
+++ components/ClassIcon.tsx
@@ -3,22 +3,22 @@ import { Tag } from '@/models'
 
 const icons: Readonly<Record<Tag, JSX.Element>> = {
   [Tag.Fighter]: (
-    <img src="img/Fighter_icon.webp" alt="Fighter icon" className="h-8 w-8" />
+    <img src={`${process.env.NEXT_PUBLIC_PATH_PREFIX}/img/Fighter_icon.webp`} alt="Fighter icon" className="h-8 w-8" />
   ),
   [Tag.Tank]: (
-    <img src="img/Tank_icon.webp" alt="Tank icon" className="h-8 w-8" />
+    <img src={`${process.env.NEXT_PUBLIC_PATH_PREFIX}/img/Tank_icon.webp`} alt="Tank icon" className="h-8 w-8" />
   ),
   [Tag.Mage]: (
-    <img src="img/Mage_icon.webp" alt="Mage icon" className="h-8 w-8" />
+    <img src={`${process.env.NEXT_PUBLIC_PATH_PREFIX}/img/Mage_icon.webp`} alt="Mage icon" className="h-8 w-8" />
   ),
   [Tag.Assassin]: (
-    <img src="img/Slayer_icon.webp" alt="Assassin icon" className="h-8 w-8" />
+    <img src={`${process.env.NEXT_PUBLIC_PATH_PREFIX}/img/Slayer_icon.webp`} alt="Assassin icon" className="h-8 w-8" />
   ),
   [Tag.Support]: (
-    <img src="img/Controller_icon.webp" alt="Support icon" className="h-8 w-8" />
+    <img src={`${process.env.NEXT_PUBLIC_PATH_PREFIX}/img/Controller_icon.webp`} alt="Support icon" className="h-8 w-8" />
   ),
   [Tag.Marksman]: (
-    <img src="img/Marksman_icon.webp" alt="Marksman icon" className="h-8 w-8" />
+    <img src={`${process.env.NEXT_PUBLIC_PATH_PREFIX}/img/Marksman_icon.webp`} alt="Marksman icon" className="h-8 w-8" />
   ),
 }
 
