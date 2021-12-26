264bd3105bfb7415407ea39cf6a096100a62e571
Brian Lai <brianlai30@outlook.com>
2021-12-26 15:31:19 -0800

    fix: home button icon in header

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -10,7 +10,7 @@ const Header = () => {
           <Link href="/">
             <a id="home-link" className="text-gray-500 dark:text-gray-400 hover:text-gray-800 dark:hover:text-gray-50 transition-colors rounded-full focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-indigo-500">
               <span className="sr-only">Home</span>
-              <ChestIcon />
+              <ChestIcon className="h-8 w-8" />
               {/* <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" viewBox="0 0 20 20" fill="currentColor">
                 <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
               </svg> */}
