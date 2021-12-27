784b42c33bcd26714b270a3840d0f05aeb661da1
Brian Lai <brianlai30@outlook.com>
2021-12-27 12:51:25 -0800

    refactor: move project files into src folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}'
+    './src/**/*.{js,ts,jsx,tsx}',
   ],
   darkMode: 'class',
   theme: {
