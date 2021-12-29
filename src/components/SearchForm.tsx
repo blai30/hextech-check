814b04e996755909f68773e395d0d95b5b7ee335
Brian Lai <brianlai30@outlook.com>
2021-12-29 11:35:37 -0800

    feat: add loading spinner for search

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,6 +1,7 @@
 import { Fragment, useState } from 'react'
 import { useRouter } from 'next/router'
 import { Listbox, Transition } from '@headlessui/react'
+import useRouterLoading from '@/lib/useRouterLoading'
 
 const regions: Readonly<Record<string, string>> = {
   'NA': 'North America',
@@ -21,6 +22,7 @@ const SearchForm = () => {
   const { region: selectedRegion, summoner: summonerName } = router.query as { region: string, summoner: string }
   const [summoner, setName] = useState<string>(summonerName ?? '')
   const [region, setRegion] = useState<string>(selectedRegion ?? 'NA')
+  const isLoading = useRouterLoading()
 
   const handleChangeName = (event: React.ChangeEvent<HTMLInputElement>) => {
     setName(event.target.value)
@@ -28,6 +30,7 @@ const SearchForm = () => {
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
+    console.log(isLoading)
     router.push({
       pathname: '/[region]/[summoner]',
       query: {
@@ -119,9 +122,16 @@ const SearchForm = () => {
           disabled={!region || !summoner}
           className="inline-flex items-center px-3 py-2 rounded-r-md transition-colors text-black dark:text-white bg-gray-100 dark:bg-gray-800 border border-l-0 border-gray-300 dark:border-gray-600 hover:bg-gray-200 dark:hover:bg-gray-700 focus-visible:outline-none focus-visible:ring-inset focus-visible:ring-2 focus-visible:ring-indigo-500 disabled:cursor-not-allowed disabled:opacity-60 disabled:text-gray-400 disabled:dark:text-gray-500 disabled:hover:bg-gray-100 disabled:dark:hover:bg-gray-800"
         >
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
-            <path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd" />
-          </svg>
+          {isLoading ? (
+            <svg className="animate-spin h-6 w-6 transition-colors " xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+              <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+              <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+            </svg>
+          ) : (
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
+              <path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd" />
+            </svg>
+          )}
         </button>
 
       </div>
---
+++ src/lib/useRouterLoading.tsx
@@ -0,0 +1,23 @@
+import Router from 'next/router'
+import { useState, useEffect } from 'react'
+
+const useRouterLoading = () => {
+  const [loading, setLoading] = useState(false)
+  useEffect(() => {
+    const start = () => setLoading(true)
+    const end = () => setLoading(false)
+
+    Router.events.on('routeChangeStart', start)
+    Router.events.on('routeChangeComplete', end)
+    Router.events.on('routeChangeError', end)
+
+    return () => {
+      Router.events.off('routeChangeStart', start)
+      Router.events.off('routeChangeComplete', end)
+      Router.events.off('routeChangeError', end)
+    }
+  }, [])
+  return loading
+}
+
+export default useRouterLoading
