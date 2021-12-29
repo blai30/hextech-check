ce7ca0a600a3c5c73e513d0c337ddcc3daea521b
Brian Lai <brianlai30@outlook.com>
2021-12-29 11:07:01 -0800

    refactor: change regions type and add persistence

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -2,24 +2,25 @@ import { Fragment, useState } from 'react'
 import { useRouter } from 'next/router'
 import { Listbox, Transition } from '@headlessui/react'
 
-const regions = [
-  { value: 'NA', label: 'North America' },
-  { value: 'EUW', label: 'Europe West' },
-  { value: 'EUNE', label: 'Europe Nordic & East' },
-  { value: 'KR', label: 'Korea' },
-  { value: 'BR', label: 'Brazil' },
-  { value: 'JP', label: 'Japan' },
-  { value: 'RU', label: 'Russia' },
-  { value: 'OCE', label: 'Oceania' },
-  { value: 'TR', label: 'Turkey' },
-  { value: 'LAN', label: 'Latin America North' },
-  { value: 'LAS', label: 'Latin America South' }
-]
+const regions: Readonly<Record<string, string>> = {
+  'NA': 'North America',
+  'EUW': 'Europe West',
+  'EUNE': 'Europe Nordic & East',
+  'KR': 'Korea',
+  'BR': 'Brazil',
+  'JP': 'Japan',
+  'RU': 'Russia',
+  'OCE': 'Oceania',
+  'TR': 'Turkey',
+  'LAN': 'Latin America North',
+  'LAS': 'Latin America South',
+}
 
 const SearchForm = () => {
   const router = useRouter()
-  const [summoner, setName] = useState('')
-  const [region, setRegion] = useState(regions[0])
+  const { region: selectedRegion, summoner: summonerName } = router.query as { region: string, summoner: string }
+  const [summoner, setName] = useState<string>(summonerName ?? '')
+  const [region, setRegion] = useState<string>(selectedRegion ?? 'NA')
 
   const handleChangeName = (event: React.ChangeEvent<HTMLInputElement>) => {
     setName(event.target.value)
@@ -31,7 +32,7 @@ const SearchForm = () => {
       pathname: '/[region]/[summoner]',
       query: {
         summoner,
-        region: region.value
+        region,
       }
     })
   }
@@ -49,7 +50,7 @@ const SearchForm = () => {
               <div className="relative">
                 <Listbox.Button id="region-select" className="inline-flex items-center w-24 rounded-l-md transition-colors text-black dark:text-white bg-gray-100 dark:bg-gray-800 hover:bg-gray-200 dark:hover:bg-gray-700 border border-r-0 border-gray-300 dark:border-gray-600 pl-3 pr-8 py-2 cursor-default focus-visible:outline-none focus-visible:ring-inset focus-visible:ring-2 focus-visible:ring-indigo-500">
                   <span className="flex items-center">
-                    <span className="block">{region.value}</span>
+                    <span className="block">{region}</span>
                   </span>
                   <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -66,22 +67,22 @@ const SearchForm = () => {
                   leaveTo="opacity-0 scale-95"
                 >
                   <Listbox.Options className="absolute z-20 mt-1 w-72 left-0 backdrop-blur-lg bg-white/50 dark:bg-gray-700/50 shadow-lg max-h-56 rounded-md py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm">
-                    {regions.map((region) => (
+                    {Object.keys(regions).map((region, index) => (
                       <Listbox.Option
-                        key={region.value}
+                        key={index}
+                        id={`region-${index}`}
                         className={({ active }) =>
-                          `${active ? 'text-white dark:text-black bg-indigo-600 dark:bg-indigo-400' : 'text-gray-900 dark:text-gray-100'}
-                          cursor-default select-none relative py-2 pl-2 pr-8 transition-colors ease-in-out duration-75`}
+                          `${active ? 'text-white dark:text-black bg-indigo-600 dark:bg-indigo-400' : 'text-gray-900 dark:text-gray-100'} cursor-default select-none relative py-2 pl-2 pr-8 transition-colors ease-in-out duration-75`}
                         value={region}
                       >
                         {({ selected, active }) => (
                           <>
                             <div className="flex items-center">
                               <span className="w-12 text-right font-bold">
-                                {region.value}
+                                {region}
                               </span>
                               <span className={`${selected ? 'font-semibold' : 'font-normal'} ml-4 block`}>
-                                {region.label}
+                                {regions[region]}
                               </span>
                             </div>
 
