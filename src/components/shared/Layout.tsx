19c7424e191887b3ccf095141461a9890fa61d91
Brian Lai <brianlai30@outlook.com>
2022-02-02 12:19:58 -0800

    fix: adjust layout margins and padding

--- src/components/shared/Layout.tsx
+++ src/components/shared/Layout.tsx
@@ -3,8 +3,8 @@ import { Footer, Header } from '@/components/shared'
 
 const Layout = ({ children }: React.PropsWithChildren<{}>) => {
   return (
-    <main id="layout" className="">
-      <div className="container mx-auto my-2 flex min-h-screen grow flex-col gap-6 p-4 print:mx-0 print:max-w-none print:p-0">
+    <main id="layout" className="container mx-auto">
+      <div className="flex min-h-screen grow flex-col gap-6 px-4 py-6 print:mx-0 print:max-w-none print:p-0">
         <Header />
         {children}
         <Footer />
