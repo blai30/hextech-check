4e3a2202af36e652db5b16e05dd67423c05be55d
Brian Lai <brianlai30@outlook.com>
2023-08-12 18:37:47 -0700

    Update new card design, comment out flipping for now

--- components/MasteryCard.tsx
+++ components/MasteryCard.tsx
@@ -9,20 +9,20 @@ import { Switch } from '@headlessui/react'
 
 // prettier-ignore
 const tagClasses: Readonly<Record<Tag, string>> = {
-  [Tag.Fighter]: 'text-amber-400 bg-gradient-to-br from-gray-900 to-amber-900',
-  [Tag.Tank]: 'text-indigo-400 bg-gradient-to-br from-gray-900 to-indigo-900',
-  [Tag.Mage]: 'text-sky-400 bg-gradient-to-br from-gray-900 to-sky-900',
-  [Tag.Assassin]: 'text-red-400 bg-gradient-to-br from-gray-900 to-red-900',
-  [Tag.Support]: 'text-teal-400 bg-gradient-to-br from-gray-900 to-teal-900',
-  [Tag.Marksman]: 'text-green-400 bg-gradient-to-br from-gray-900 to-green-900',
+  [Tag.Fighter]: 'text-amber-400',
+  [Tag.Tank]: 'text-indigo-400',
+  [Tag.Mage]: 'text-sky-400',
+  [Tag.Assassin]: 'text-red-400',
+  [Tag.Support]: 'text-teal-400',
+  [Tag.Marksman]: 'text-green-400',
 }
 
 // prettier-ignore
 const masteryClasses = (masteryLevel: number) => ({
-  [5]: 'text-red-100 from-red-900',
-  [6]: 'text-fuchsia-100 from-fuchsia-900',
-  [7]: 'text-green-100 from-green-900',
-})[masteryLevel] ?? 'text-gray-100 from-gray-900'
+  [5]: 'text-rose-300',
+  [6]: 'text-fuchsia-300',
+  [7]: 'text-emerald-300',
+})[masteryLevel] ?? 'text-gray-300'
 
 export default function MasteryCard({
   champion,
@@ -49,7 +49,7 @@ export default function MasteryCard({
       }}
     >
       {/* Flip switch */}
-      <Switch
+      {/* <Switch
         className="group absolute flex h-full w-full cursor-default items-center justify-center outline-none"
         checked={flipped}
         onChange={setFlipped}
@@ -69,118 +69,108 @@ export default function MasteryCard({
             />
           </svg>
         </div>
-      </Switch>
+      </Switch> */}
 
       {/* Card container */}
-      <div
-        className="relative aspect-[3/2] h-full w-full transform-gpu transition-transform duration-200 ease-in-out xs:aspect-[3/4]"
+      {/* <div
+        className="relative h-full w-full transform-gpu transition-transform duration-200 ease-in-out"
         style={{
           transformStyle: 'preserve-3d',
           transform: flipped ? 'rotateY(180deg)' : 'unset',
         }}
+      > */}
+      {/* Front side */}
+      <div
+        className="relative flex h-full w-full flex-col justify-end overflow-hidden rounded-xl shadow-lg"
+        style={{
+          WebkitBackfaceVisibility: 'hidden',
+          backfaceVisibility: 'hidden',
+        }}
       >
-        {/* Front side */}
+        <Image
+          src={imageUrl}
+          alt={`Champion ${champion.name} loading screen image`}
+          fill
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          className="absolute inset-0 -z-10 h-full w-full object-cover"
+        />
+        <div className="absolute inset-0 -z-10 bg-gradient-to-t from-gray-900 via-gray-900/40" />
         <div
           className={[
-            'absolute h-full w-full overflow-hidden rounded-xl bg-transparent shadow-lg ring-4 ring-inset',
+            'absolute inset-0 -z-10 m-2 rounded-xl border',
             mastery.chestGranted
-              ? 'ring-yellow-300 dark:ring-yellow-500'
-              : 'ring-gray-400 dark:ring-gray-900',
+              ? 'border-yellow-400/60'
+              : 'border-gray-400/40',
           ].join(' ')}
-          style={{
-            WebkitBackfaceVisibility: 'hidden',
-            backfaceVisibility: 'hidden',
-          }}
-        >
-          <Image
-            src={imageUrl}
-            alt={`Champion ${champion.name} loading screen image`}
-            fill
-            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-            style={{
-              objectFit: 'cover',
-            }}
-            className="-z-10 rounded-lg"
-          />
-          <div className="flex h-full w-full flex-col justify-between">
-            <div className="flex flex-row items-center justify-between">
-              <div className="flex flex-row gap-2 pl-2">
-                {champion.tags.map((tag) => (
-                  <button
-                    key={tag}
-                    id={`${champion.id}-tag-${tag}`}
-                    className={[
-                      'group relative flex flex-col items-center justify-center rounded-b-full bg-gray-800 p-2',
-                      tagClasses[tag],
-                    ].join(' ')}
-                  >
-                    <div className="absolute -bottom-12 left-0 hidden whitespace-nowrap rounded px-4 py-2 font-body text-base font-medium text-white shadow-xl backdrop-blur-lg backdrop-brightness-50 group-hover:block group-focus:block print:hidden print:group-hover:hidden print:group-focus:hidden">
-                      <div className="flex flex-col items-center gap-y-1">
-                        <p id={`${champion.id}-tag-${Tag[tag]}-tooltip`}>
-                          {Tag[tag]}
-                        </p>
-                      </div>
-                    </div>
-                    <ClassIcon className="h-6 w-6" tag={tag} />
-                  </button>
-                ))}
-              </div>
-              <div
-                id={`champion-chest-${champion.id}`}
-                title={`Chest ${
-                  mastery.chestGranted ? 'obtained' : 'available'
-                }`}
+        />
+
+        <div className="flex flex-col gap-2 px-6 py-5">
+          <div className="flex gap-3">
+            {champion.tags.map((tag) => (
+              <button
+                key={tag}
+                id={`${champion.id}-tag-${tag}`}
                 className={[
-                  'rounded-bl-xl p-2',
-                  mastery.chestGranted
-                    ? 'bg-yellow-300 text-yellow-700 dark:bg-yellow-500 dark:text-yellow-900'
-                    : 'bg-gray-400 text-white dark:bg-gray-900 dark:text-gray-400',
+                  'group relative flex flex-col items-center justify-center rounded-full',
+                  tagClasses[tag],
                 ].join(' ')}
               >
-                <ChestIcon className="h-6 w-6" />
-              </div>
-            </div>
-            <div className="flex flex-col gap-2">
-              <p
+                <div className="absolute -top-12 left-0 hidden whitespace-nowrap rounded px-4 py-2 font-body text-base font-medium text-white shadow-xl backdrop-blur-lg backdrop-brightness-50 group-hover:block group-focus:block print:hidden print:group-hover:hidden print:group-focus:hidden">
+                  <div className="flex flex-col items-center gap-y-1">
+                    <p id={`${champion.id}-tag-${Tag[tag]}-tooltip`}>
+                      {Tag[tag]}
+                    </p>
+                  </div>
+                </div>
+                <ClassIcon className="h-6 w-6" tag={tag} />
+              </button>
+            ))}
+          </div>
+          <p className="text-left text-2xl font-thin text-white subpixel-antialiased 2xs:text-4xl xs:text-3xl sm:text-2xl md:text-2xl">
+            {champion.name}
+          </p>
+          <div
+            className={[
+              'flex items-center justify-between gap-2',
+            ].join(' ')}
+          >
+            <div className="flex items-center gap-1">
+              <span
+                id={`champion-level-${champion.id}`}
+                title="Mastery level"
                 className={[
-                  'translate-y-3.5 rounded-xl text-left font-display text-4xl font-extrabold uppercase italic tracking-wide subpixel-antialiased 2xs:text-5xl xs:text-4xl sm:text-3xl md:text-4xl',
-                  mastery.chestGranted
-                    ? 'text-yellow-300 dark:text-yellow-500'
-                    : 'text-gray-400 dark:text-gray-900',
-                ].join(' ')}
-              >
-                {champion.name}
-              </p>
-              <div
-                className={[
-                  'mx-1 mb-1 flex flex-row items-center justify-between rounded-b-lg bg-gradient-to-r via-transparent to-gray-900 px-1.5 pb-2 pt-1 backdrop-blur-md backdrop-brightness-200',
                   masteryClasses(mastery.championLevel),
+                  'text-lg font-normal transition w-4 text-left',
                 ].join(' ')}
               >
-                <span
-                  id={`champion-level-${champion.id}`}
-                  title="Mastery level"
-                  className={[
-                    masteryClasses(mastery.championLevel),
-                    'flex h-8 w-8 items-center justify-center rounded-full font-display text-2xl font-extrabold uppercase italic transition',
-                  ].join(' ')}
-                >
-                  {mastery.championLevel}
-                </span>
-                <span
-                  id={`champion-points-${champion.id}`}
-                  title="Mastery points"
-                  className="flex items-center justify-center bg-gradient-to-b from-yellow-200 via-yellow-200 to-yellow-400 bg-clip-text px-2.5 font-display text-2xl font-bold uppercase italic text-transparent transition"
-                >
-                  {mastery.championPoints.toLocaleString()}
-                </span>
-              </div>
+                {mastery.championLevel}
+              </span>
+              <span className="text-lg font-thin text-white transition w-2.5 text-left">|</span>
+              <span
+                id={`champion-points-${champion.id}`}
+                title="Mastery points"
+                className="bg-gradient-to-b from-yellow-100 via-yellow-100 to-yellow-200 bg-clip-text text-lg text-transparent transition text-left"
+              >
+                {mastery.championPoints.toLocaleString()}
+              </span>
             </div>
+            {/* <div
+              id={`champion-chest-${champion.id}`}
+              title={`Chest ${mastery.chestGranted ? 'obtained' : 'available'}`}
+              className={[
+                mastery.chestGranted
+                  ? 'text-yellow-300 dark:text-yellow-400'
+                  : 'text-white dark:text-gray-400',
+              ].join(' ')}
+            >
+              <ChestIcon className="h-6 w-6" />
+            </div> */}
           </div>
         </div>
+      </div>
 
-        {/* Back side */}
-        <div
+      {/* Back side */}
+      {/* <div
           className={[
             'absolute h-full w-full overflow-hidden rounded-xl bg-transparent shadow-lg',
           ].join(' ')}
@@ -239,8 +229,8 @@ export default function MasteryCard({
               <p>{'Last played ' + formatRelativeDate(lastPlayed)}</p>
             </div>
           </div>
-        </div>
-      </div>
+        </div> */}
+      {/* </div> */}
     </li>
   )
 }
